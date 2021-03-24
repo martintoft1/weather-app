2b3c3ea614bf54fab5439a28528f5ebfd52375c3
ChristianOlsen <christian.tkd.olsen@gmail.com>
2021-03-24 04:44:07 +0100

    Added dependencies, og tokens for mapbox + View for map

--- app/src/main/java/com/team48/applikasjon/MainActivity.kt
+++ app/src/main/java/com/team48/applikasjon/MainActivity.kt
@@ -2,10 +2,34 @@ package com.team48.applikasjon
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.gson.Gson
+import com.mapbox.mapboxsdk.Mapbox
+import com.mapbox.mapboxsdk.maps.MapView
+import com.mapbox.mapboxsdk.maps.Style
 
 class MainActivity : AppCompatActivity() {
+
+    private var mapView: MapView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        Mapbox.getInstance(this, getString(R.string.mapbox_access_token))
         setContentView(R.layout.activity_main)
+        loadMapbox(savedInstanceState)
+
     }
+
+    fun loadMapbox(savedInstanceState: Bundle?) {
+        mapView = findViewById(R.id.mapView)
+        mapView?.onCreate(savedInstanceState)
+        mapView?.getMapAsync { mapboxMap ->
+
+            mapboxMap.setStyle(Style.MAPBOX_STREETS) {
+
+            }
+        }
+    }
+
+
 }
