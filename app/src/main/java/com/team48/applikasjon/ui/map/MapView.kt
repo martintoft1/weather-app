ebbe5aeb7dbd702c46e6b2aebf9a8b242d6bc2ae
Simon <simoan@uio.no>
2021-04-07 21:32:04 +0200

    IN2000-58 WIP API og MVVM

--- app/src/main/java/com/team48/applikasjon/ui/map/MapView.kt
+++ app/src/main/java/com/team48/applikasjon/ui/map/MapView.kt
@@ -1,20 +1,32 @@
 package com.team48.applikasjon.ui.map
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import com.github.kittinunf.fuel.Fuel
+import com.github.kittinunf.fuel.coroutines.awaitString
+import com.google.gson.Gson
 import com.mapbox.mapboxsdk.Mapbox
 import com.mapbox.mapboxsdk.maps.MapView
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback
 import com.mapbox.mapboxsdk.maps.Style
+import com.mapbox.mapboxsdk.style.layers.Layer
 import com.team48.applikasjon.R
+import com.team48.applikasjon.data.models.MetVectorData
 import com.team48.applikasjon.data.repository.Repository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
 class MapView : Fragment(R.layout.fragment_map_view) {
 
+    private lateinit var mapViewModel: MapViewModel
     var mapView: MapView? = null
-    val mapViewModel = MapViewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,20 +38,61 @@ class MapView : Fragment(R.layout.fragment_map_view) {
         savedInstanceState: Bundle?
     ): View? {
 
+        // Forbinder View med ViewModel
+        Log.i("MapFragment", "Called ViewModelProvider.get")
+        mapViewModel = ViewModelProvider(this).get(MapViewModel::class.java)
+
+        // MapBox-relatert, skal prob flyttes til ViewModel eller MainActivity
         Mapbox.getInstance(requireContext().applicationContext, getString(R.string.mapbox_access_token))
 
+        // Oppsett av View
         val view = inflater.inflate(R.layout.fragment_map_view, container, false)
-
         mapView = view.findViewById(R.id.mapView)
         mapView?.onCreate(savedInstanceState)
-        mapView?.getMapAsync { mapboxMap ->
 
-            mapboxMap.setStyle(Style.OUTDOORS) {
 
-                // Her skal vi (kanskje?) legge til data fra MET-API
 
+        // ------------------------------------------- //
+        // TEMP LØSNING: Legger API-koden her for å teste layers i MapBox
+        // ------------------------------------------- //
+        /*
+
+        val metPath = "https://test.openmaps.met.no/in2000/map/services"
+        var metData: List<MetVectorData> = emptyList()
+
+        runBlocking {
+            try {
+                metData = Gson().fromJson(Fuel.get(metPath).awaitString(), Array<MetVectorData>::class.java).toList()
+            } catch (exception: Exception) {
+                exception.message?.let { Log.e("getting MET-data", it) }
             }
         }
+
+        val jsonString = Gson().toJson(metData[0])
+        Log.i("jsonString metData[0]", jsonString)
+
+        val styleBuild: Style.Builder = Style.Builder().fromJson(jsonString)
+
+        val layers = styleBuild.layers
+        Log.i("layers", layers.toString())
+
+         */
+        // ------------------------------------------- //
+        // ------------------------------------------- //
+
+        mapView?.getMapAsync(OnMapReadyCallback { mapboxMap ->
+
+            mapboxMap.setStyle(Style.MAPBOX_STREETS) { style ->
+
+                // Ved bruk av lambdaen(?) (style ->) så kan vi legge til data til MAXBOX_STREETS style
+
+                // Dette kan gjøres med:
+                //style.addLayer(layer)
+
+                // Men inntil videre er variabelen layers på linje 76 tom, ukjent hvorfor
+
+            }
+        })
 
         return view
     }
--- app/src/main/java/com/team48/applikasjon/ui/map/MapViewModel.kt
+++ app/src/main/java/com/team48/applikasjon/ui/map/MapViewModel.kt
@@ -1,8 +1,20 @@
 package com.team48.applikasjon.ui.map
 
-class MapViewModel {
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import com.team48.applikasjon.data.api.ApiHelper
+import com.team48.applikasjon.data.api.ApiService
+import com.team48.applikasjon.data.repository.Repository
 
+class MapViewModel : ViewModel() {
 
+    init {
+        Log.i("MapViewModel", "MapViewModel created!")
+    }
 
-
+    // Kalles før ViewModel blir destroyed (ved fragment detaching eller activity finished)
+    override fun onCleared() {
+        super.onCleared()
+        Log.i("MapViewModel", "MapViewModel destroyed!")
+    }
 }
