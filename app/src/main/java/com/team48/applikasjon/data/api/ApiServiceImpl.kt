9938c284b9d67ca91a2bf0a2938549d20d675e8e
ludvigmc <ludvigmc@ifi.uio.no>
2021-04-13 22:22:20 +0200

    IN2000-73 Added comments

--- app/src/main/java/com/team48/applikasjon/data/api/ApiServiceImpl.kt
+++ app/src/main/java/com/team48/applikasjon/data/api/ApiServiceImpl.kt
@@ -4,9 +4,12 @@ import com.rx2androidnetworking.Rx2AndroidNetworking
 import com.team48.applikasjon.data.models.Weather
 import io.reactivex.Single
 
+// Klarte ikke opprette instans av ApiService direkte, så kanskje er ikke denne klassen helt ubrukelig likevel? Idk
+
 class ApiServiceImpl : ApiService {
 
     override fun getWeather(): Single<List<Weather>> {
+                                        // Denne stemmer vel ikke helt eeee?
         return Rx2AndroidNetworking.get("https://5e510330f2c0d300147c034c.mockapi.io/users")
             .build()
             .getObjectListSingle(Weather::class.java)
