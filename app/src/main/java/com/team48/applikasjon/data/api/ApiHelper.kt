69f5b9d1a72fab9bcd141a2a47743fa1f8228c29
ChristianOlsen <christian.tkd.olsen@gmail.com>
2021-04-23 09:34:41 +0200

    IN2000-79 Små endringer i kode. Vektorkart fungerer med repo. Lagt til TODO i MapFragment.kt

--- app/src/main/java/com/team48/applikasjon/data/api/ApiHelper.kt
+++ app/src/main/java/com/team48/applikasjon/data/api/ApiHelper.kt
@@ -1,8 +1,5 @@
 package com.team48.applikasjon.data.api
 
-import com.team48.applikasjon.data.models.VectorTile
-import io.reactivex.Single
-
 class ApiHelper (private  val apiServiceImpl : ApiServiceImpl) {
 
    fun getAirTemp() = apiServiceImpl.getAirTemp()
--- app/src/main/java/com/team48/applikasjon/ui/map/MapFragment.kt
+++ app/src/main/java/com/team48/applikasjon/ui/map/MapFragment.kt
@@ -2,25 +2,26 @@ package com.team48.applikasjon.ui.map
 
 import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
-import com.mapbox.mapboxsdk.Mapbox
 import com.mapbox.mapboxsdk.maps.MapView
 import com.mapbox.mapboxsdk.maps.Style
+import com.mapbox.mapboxsdk.style.expressions.Expression
+import com.mapbox.mapboxsdk.style.expressions.Expression.*
 import com.mapbox.mapboxsdk.style.layers.FillLayer
 import com.mapbox.mapboxsdk.style.layers.PropertyFactory.fillColor
 import com.mapbox.mapboxsdk.style.layers.PropertyFactory.fillOpacity
 import com.mapbox.mapboxsdk.style.sources.TileSet
 import com.mapbox.mapboxsdk.style.sources.VectorSource
 import com.team48.applikasjon.R
+import com.team48.applikasjon.data.models.VectorTile
 import com.team48.applikasjon.ui.main.ViewModelFactory
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
-import java.net.URI
-import java.net.URL
 
 class MapFragment(val viewModelFactory: ViewModelFactory) : Fragment() {
 
@@ -60,7 +61,8 @@ class MapFragment(val viewModelFactory: ViewModelFactory) : Fragment() {
             val customStyle = Style.Builder().fromUri(getString(R.string.mapStyleUri))
             map.setStyle(customStyle) { style ->
 
-                // TODO!! her må vi vente til apiet er lastet inn
+
+                // her må vi vente til apiet er lastet inn
                 // TODO: IMPLEMENTERE LIVEDATA FOR REPOSITORY ELLER APISERVICEIMPL
                 // runblocking på 5 sekunder er midlertidig løsning
                 runBlocking {
@@ -69,10 +71,23 @@ class MapFragment(val viewModelFactory: ViewModelFactory) : Fragment() {
 
                 val repository = mapViewModel.repository
 
-                val airTempTile = repository.getAirTemp()[0]
+                // TODO: Lag UI for å sette dette valget av værtype som skal vises
+                var weatherType = 0;
+                lateinit var weatherTile: VectorTile
+
+                when (weatherType) {
+                    0 -> weatherTile = repository.getAirTemp()[0]
+                    1 -> weatherTile = repository.getClouds()[0]
+                    2 -> weatherTile = repository.getPrecipitation()[0]
+                    3 -> weatherTile = repository.getPressure()[0]
+                }
+
+
                 // Adding source to style
 
-                val tileSet = TileSet(airTempTile.tilejson, airTempTile.tiles?.get(0))
+                val tileSet = TileSet(weatherTile.tilejson, weatherTile.tiles?.get(0))
+
+
                 val vectorSource = VectorSource("weatherData", tileSet)
                 style.addSource(vectorSource)
 
@@ -80,20 +95,72 @@ class MapFragment(val viewModelFactory: ViewModelFactory) : Fragment() {
                 val fillLayer = FillLayer("airTemp", "weatherData")
 
                 // TODO: Create better presentation based on weather type
+                /*
                 fillLayer.setProperties(
-                        fillColor(Color.BLUE),
-                        fillOpacity(0.4f)
+                    fillColor(Color.BLUE),
+                    fillOpacity(0.4f),
                 )
-                fillLayer.sourceLayer = airTempTile.getTileId()
+
+                 */
+/*
+                fillLayer.setProperties(
+                    fillOpacity(0.4F),
+                    fillColor(
+                        interpolate( linear(), zoom(),
+                            stop(12, step(get("stroke-width"),
+                                color(Color.YELLOW),
+                                stop(-20f, color(Color.RED)),
+                                stop(0f, color(Color.WHITE)),
+                                stop(20f, color(Color.BLUE))
+                            ))
+                        )
+                    )
+                )
+ */
+                fillLayer.setProperties(
+                    fillOpacity(0.4F),
+                    fillColor(interpolate(
+                        linear(), zoom(),
+                        stop(-20f, color(Color.RED)),
+                        stop(0f, color(Color.WHITE)),
+                        stop(20f, color(Color.BLUE))
+                    ))
+                )
+                Log.d("tileset", fillLayer.toString())
+
+
+
+
+                // Spesifiserer hvilke zoomnivåer dataen vil være tilgjengelig
+                fillLayer.minZoom = weatherTile.minzoom!!.toFloat()
+                fillLayer.maxZoom = weatherTile.maxzoom!!.toFloat()
+
+                fillLayer.sourceLayer = weatherTile.getTileId()
 
                 // Adding layer to style
                 style.addLayer(fillLayer)
 
+                //Log.d("minZoom", fillLayer.)
+
+
+            }
+        }
+/*
+        mapView?.getMapAsync{ map ->
+
+            map.addOnMapClickListener {
+                Log.d("latlng", it.toString())
             }
         }
 
+ */
+
+
+
         return rootView
     }
+
+
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
