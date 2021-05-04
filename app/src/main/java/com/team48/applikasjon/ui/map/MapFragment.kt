a231bd0f802258b590127f0ecb01b7e750d59934
Christian Olsen <chrolsen@uio.no>
2021-05-04 18:35:54 +0200

    getWeatherFrom punkt

--- app/src/main/java/com/team48/applikasjon/ui/map/MapFragment.kt
+++ app/src/main/java/com/team48/applikasjon/ui/map/MapFragment.kt
@@ -107,17 +107,18 @@ class MapFragment(val viewModelFactory: ViewModelFactory) : Fragment() {
         }
     }
 
-
     fun getWeatherFrom(map: MapboxMap, point: LatLng) {
         // Convert LatLng coordinates to screen pixel and only query the rendered features.
         val pixel = map.projection.toScreenLocation(point)
-        var dataArr = arrayOfNulls<String>(3)
+        var dataArr = arrayOfNulls<Float>(3)
 
         if (map.queryRenderedFeatures(pixel, "layer1","layer2","layer3").size > 0) {
             for (i in dataArr.indices) {
                 val jsonData = map.queryRenderedFeatures(pixel, "layer${i+1}")
                 if (jsonData.size > 0) {
-                    dataArr[i] = jsonData[0].properties()!!["value"].toString()
+                    dataArr[i] = jsonData[0].properties()!!["value"].toString().toFloat()
+                } else {
+                    dataArr[i] = 0F
                 }
             }
         } else {
