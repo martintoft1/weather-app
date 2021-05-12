774c1aa80487c0ff4daaafef8ca190a8469ad74d
matoft <martinolsson@live.no>
2021-05-12 22:05:01 +0200

    Endret på tekstbeskrivelse som gis ved ulikt vær, og la til en samlende tekstbeskrivelse for alle de 3 værtypene

--- app/src/main/java/com/team48/applikasjon/utils/WeatherConverter.kt
+++ app/src/main/java/com/team48/applikasjon/utils/WeatherConverter.kt
@@ -2,8 +2,8 @@ package com.team48.applikasjon.utils
 
 class WeatherConverter() {
     val cloudDescs = listOf("Ingen skyer", "Delvis skyet", "Overskyet")
-    val rainDescs  = listOf("Ingen nedbør", "Litt regn", "Mye regn")
-    val tempDescs  = listOf("Iskaldt!", "Kjølig", "Godt og varmt")
+    val rainDescs  = listOf("Ingen nedbør", "Oppholdsvær", "Regn", "Pøsregn!")
+    val tempDescs  = listOf("Iskaldt!", "Kjølig", "Lunkent", "Godt og varmt", "Kokvarmt!")
 
 
     fun getCloudDesc(value: Float?) : String {
@@ -22,8 +22,9 @@ class WeatherConverter() {
 
         return when {
             value <= 0.0 -> rainDescs[0] + " ($value mm)"
-            value <= 0.5 -> rainDescs[1] + " ($value mm)"
-            else         -> rainDescs[2] + " ($value mm)"
+            value <= 0.4 -> rainDescs[1] + " ($value mm)"
+            value <= 0.8 -> rainDescs[2] + " ($value mm)"
+            else         -> rainDescs[3] + " ($value mm)"
         }
     }
 
@@ -32,9 +33,34 @@ class WeatherConverter() {
         if (value == null) return tempDescs[0] + " ($value °C)"
 
         return when {
-            value <= 0.0 -> tempDescs[0] + " ($value °C)"
-            value <= 10  -> tempDescs[1] + " ($value °C)"
-            else         -> tempDescs[2] + " ($value °C)"
+            value < -5.0 -> tempDescs[0] + " ($value °C)"
+            value < 10.0 -> tempDescs[1] + " ($value °C)"
+            value < 20  -> tempDescs[2] + " ($value °C)"
+            value < 30  -> tempDescs[3] + " ($value °C)"
+            else         -> tempDescs[4] + " ($value °C)"
+        }
+    }
+
+    fun getWeatherDesc(cloud: Float?, rain: Float?, temp: Float?) : String {
+        // Sørger for å ha verdier å sammenlikne selv om noen av verdiene skulle være 0
+        val cloudVar: Float
+        val rainVar: Float
+        val tempVar: Float
+
+        cloudVar = if (cloud == null) 0.toFloat() else cloud
+        rainVar = if (rain == null) 0.toFloat() else rain
+        tempVar = if (temp == null) 0.toFloat() else temp
+
+        return when {
+            rainVar > 0.4 && tempVar < 10 && tempVar >= -2 -> "Klesanbefaling: varmt utetøy som tåler vann, og varme støvler"
+            rainVar > 0.4 && tempVar >= 10 -> "Klesanbefaling: regntøy og støvler"
+            tempVar < -5 -> "Klesanbefaling: skikkelig varmt utetøy"
+            tempVar < 5 -> "Klesanbefaling: varmt utetøy"
+            tempVar < 15 -> "Klesanbefaling: jakke, lue, skjerf og hansker"
+            tempVar < 20 -> "Klesanbefaling: genser og bukse"
+            tempVar < 25 -> "Klesanbefaling: bukse og t-skjorte"
+
+            else -> "Klesanbefaling: short og t-skjorte"
         }
     }
 }
