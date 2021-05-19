c3019892dbf5c0eaaadcd1b8baf4584cdade1f9d
ludvigmc <ludvigmc@ifi.uio.no>
2021-05-19 12:05:34 +0200

    polering av lort

--- app/src/main/java/com/team48/applikasjon/ui/main/SplashActivity.kt
+++ app/src/main/java/com/team48/applikasjon/ui/main/SplashActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.os.PersistableBundle
+import android.view.Window
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import com.airbnb.lottie.LottieAnimationView
 import com.team48.applikasjon.R
@@ -28,13 +30,14 @@ class SplashActivity : AppCompatActivity() {
 
         supportActionBar?.hide()
 
+
         //added the variable for animation-view, in case of later use
         lottieAnimationView = findViewById(R.id.animatedLogo)
 
         //Uncertain for the use-case of this (not in use)
         //lottieAnimationView.animate().translationY(1400F).setDuration(1000).setStartDelay(4000)
 
-        CoroutineScope(Dispatchers.IO).launch {  }
+       // CoroutineScope(Dispatchers.IO).launch {  }
 
 
         Handler(Looper.getMainLooper()).postDelayed({
